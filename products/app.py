eeff76dda59e87f71f566171ee03ce5d25611d7f
semantoDevelopers <rionhacker@gmail.com>
2021-05-10 17:56:57 +0200

    Adding products

--- products/app.py
+++ products/app.py
@@ -5,18 +5,39 @@ products = Blueprint('products',__name__)
 
 db_products = DatabaseProducts()
 
+
+
+#Route for getting all products
 @products.route('/',methods=['GET'])
 def get_all_products():
     pass
 
+#Route for get filtered products by storeid
 @products.route('/<int:storeid>',methods=['GET'])
 def get_products_of_store(storeid):
     pass
 
+#Route for get filtered products by category id
 @products.route('/<int:catid>',methods=['GET'])
 def get_products_of_category(catid):
     pass
 
+#Route for getting categories
 @products.route('/categories',methods=['GET'])
 def get_categories():
     pass
+
+
+##ADMIN ROUTES##
+
+
+#Route for posting products
+@products.route('/', methods=['POST'])
+def post_products():
+    pass
+
+#Route for posting categories
+@products.route('/categories',methods=['POST'])
+def post_categories():
+    pass
+
--- products/db_products.py
+++ products/db_products.py
@@ -9,3 +9,13 @@ class DatabaseProducts:
 
     def config(self, app):
         self.mysql.init_app(app)
+
+    def getConnection(self):
+        conn = self.mysql.connect()
+        cursor = conn.cursor()
+        return conn, cursor
+
+    def register_product(self,data):
+        conn,cursor = self.getConnection()
+        query = "INSERT INTO products(name,price,categories_id,models_id,vendors_id,media_id,is_available,is_stocked,stock_quantity,data_created) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        pass    
