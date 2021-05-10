a2efbb914d2fab6e6c63a126a75b796d361e40cb
semantoDevelopers <rionhacker@gmail.com>
2021-05-10 13:54:59 +0200

    Bug Fixed db user

--- user/db_user.py
+++ user/db_user.py
@@ -17,12 +17,13 @@ class DatabaseUser:
         return conn, cursor
 
     def check_if_data_is_retrieved(self, data):
-	print(len(data))
+
         if len(data) < 1:
-		print(len(data))
+            print(len(data))
+            print(data)
             return False
         else:
-		print(len(data))
+            print(len(data))
             return True
 
     def get_user_data(self, email, password):
@@ -30,8 +31,10 @@ class DatabaseUser:
         query = "SELECT * FROM users WHERE email=%s AND password=%s"
         tuple = (shaCryptData(email), shaCryptData(password))
         cursor.execute(query, tuple)
-        if(self.check_if_data_is_retrieved(cursor.fetchall())):
-            data = cursor.fetchall()[0]
+        fetchedData = cursor.fetchall()
+        if(self.check_if_data_is_retrieved(fetchedData)):
+
+            data = fetchedData[0]
             conn.close()
             return {'error': None, 'user': {'id': data[0], 'secret_key': data[1], 'name': data[2], 'surname': data[3], 'email': data[4], 'phone_number': data[6], 'address': data[7], 'fiscal_code': data[8], 'favorites': data[9], 'media_id': data[10]}}
         else:
