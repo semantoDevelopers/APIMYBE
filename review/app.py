679f89a0ff351068377326f76c2283473fefeb10
ghost0r10n <rionhacker@gmail.com>
2021-07-09 11:20:45 +0200

    TestBugs

--- review/app.py
+++ review/app.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,request
 from .db_review import DatabaseReview
+from utils.checkheaders import giveResponse
 from dotenv import load_dotenv
 import os
 
@@ -12,11 +13,12 @@ db_review = DatabaseReview()
 
 @review.route('/<int:prodid>',methods=['GET'])
 def get_reviews_of_prod(prodid):
-    pass
+    return giveResponse(db_review.register_review(request.json),secret_key,request)
 
 @review.route('/<int:prodid>',methods=['POST'])
 def post_review_of_prod(prodid):
     pass
+  #  return giveResponse()
 
 @review.route('/<int:prodid>/<int:userid>',methods=['PUT'])
 def modify_review_of_prod(prodid,userid):
--- review/db_review.py
+++ review/db_review.py
@@ -1,11 +1,21 @@
+from classes.Database import Database
 from flaskext.mysql import MySQL 
+from classes.Database import Database
 
 
+class DatabaseReview(Database):
 
-class DatabaseReview:
+    def register_review(self,data):
+        conn,cursor = self.getConnection()
+        query = "INSERT INTO review(comment,point,users_id,products_id) VALUES(%s,%s,%s,%s)"
+        tuple = (data['comment'],data['point'],data['users_id'],data['products_id'])
+        try:
+            cursor.execute(query,tuple)
+            conn.commit()
+            conn.close()
+            return {'error':None,'message':'Review created'}
+        except Exception as e:
+            return  {'error':str(e),'message':'There was an error on creating the review'}
 
-    def __init__(self):
-        self.mysql = MySQL()
 
-    def config(self, app):
-        self.mysql.init_app(app)
+    
